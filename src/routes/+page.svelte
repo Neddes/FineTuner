1d570756943ec342ff797c6faa4db282e7417962
Neddes <eric@neddes.com>
2023-11-29 21:02:19 +0100

    initial structure updated

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
 	import Button from '$lib/components/ui/button/button.svelte';
 	import * as Avatar from '$lib/components/ui/avatar';
-
+	import * as Table from '$lib/components/ui/table';
 	import Label from '$lib/components/ui/label/label.svelte';
 	import Separator from '$lib/components/ui/separator/separator.svelte';
 	import Textarea from '$lib/components/ui/textarea/textarea.svelte';
@@ -11,6 +11,8 @@
 	let systemMessage = 'You are a helpful assistant';
 	let userMessage = 'who are you?';
 	let stream: ReadableStream | null = null;
+
+	let fineTuneSystemMessage = '';
 
 	async function generateSyntheticData() {
 		response = '';
@@ -53,11 +55,22 @@
 			// Inside your stream processing code
 		}
 	}
+
+	let fineTuneItems = [
+		{
+			input: '',
+			output: ''
+		}
+	];
+
+	function addRow() {
+		fineTuneItems = [...fineTuneItems, { input: '', output: '' }];
+	}
 </script>
 
-<div class="hidden h-full flex-col md:flex w-full">
+<div class="hidden h-full flex-col md:flex w-full items-center">
 	<div
-		class="container flex flex-col items-start justify-between space-y-2 py-4 sm:flex-row sm:items-center sm:space-y-0 md:h-16"
+		class="container flex flex-col items-start justify-between space-y-2 py-4 sm:flex-row sm:items-center sm:space-y-0 md:h-16 max-w-7xl"
 	>
 		<h2 class="text-lg font-semibold">FineTuner</h2>
 		<div class="flex flex-row gap-x-4">
@@ -68,8 +81,61 @@
 		</div>
 	</div>
 	<Separator />
-	<div class="flex flex-row w-full">
-		<div class="bg-gray-50 w-full">
+	<div class="flex flex-row w-full pt-8 max-w-7xl">
+		<div class="w-full flex flex-col gap-y-6">
+			<div>
+				<h3 class="text-lg font-semibold">Your Finetune set</h3>
+				<p class="text-slate-600">
+					This will create a .jsonl file which you can use to finetune any Chat model. We can help
+					you generate synthetic input data. You can change those settings on the right. Click here
+					to see how this works.
+				</p>
+			</div>
+			<div class="flex flex-col w-full gap-1.5">
+				<Label for="fineTuneSystemMessage">Fine Tune System Message</Label>
+				<Textarea
+					bind:value={fineTuneSystemMessage}
+					placeholder="FineTune System Message"
+					id="fineTuneSystemMessage"
+					name="fineTuneSystemMessage"
+				/>
+			</div>
+			<div>
+				<h3 class="text-lg font-semibold">Your Input & Output Data</h3>
+				<p class="text-slate-600">
+					Every row in your file will consist of your system message (above, input and output data).
+				</p>
+			</div>
+			<Table.Root>
+				<Table.Header>
+					<Table.Row>
+						<Table.Head class="border border-gray-300 rounded-tl-lg">Input</Table.Head>
+						<Table.Head class="border border-gray-300 rounded-tr-lg">Output</Table.Head>
+					</Table.Row>
+				</Table.Header>
+				<Table.Body>
+					{#each fineTuneItems as item}
+						<Table.Row>
+							<Table.Cell class="border border-gray-300 p-2 ">
+								<Textarea
+									bind:value={item.input}
+									placeholder="Your input data here..."
+									class="border-none resize-none"
+								/>
+							</Table.Cell>
+							<Table.Cell class="border border-gray-300">
+								<Textarea
+									bind:value={item.output}
+									placeholder="Your output data here..."
+									class="border-none resize-none"
+								/>
+							</Table.Cell>
+						</Table.Row>
+					{/each}
+				</Table.Body>
+			</Table.Root>
+			<Button on:click={addRow}>Add Row</Button>
+
 			<div class="max-w-lg flex flex-col gap-y-4 p-8">
 				<Label>{completedResponse}</Label>
 				{#if completedResponse}
@@ -79,6 +145,7 @@
 		</div>
 		<div>
 			<div class="max-w-lg flex flex-col gap-y-4 p-8 min-w-[480px]">
+				<h3 class="text-lg font-semibold">FineTunerGenerate Synthetic Input Data</h3>
 				<Textarea bind:value={systemMessage} placeholder="System Message" />
 				<Textarea bind:value={userMessage} placeholder="User Message" />
 
