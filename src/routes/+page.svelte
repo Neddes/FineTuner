8470e977de928fba1d026b5cd211b897e3173349
Neddes <eric@neddes.com>
2023-11-29 20:36:25 +0100

    inital structure

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,18 @@
 <script lang="ts">
-	import Badge from '$lib/components/ui/badge/badge.svelte';
+	import Button from '$lib/components/ui/button/button.svelte';
+	import * as Avatar from '$lib/components/ui/avatar';
+
+	import Label from '$lib/components/ui/label/label.svelte';
+	import Separator from '$lib/components/ui/separator/separator.svelte';
+	import Textarea from '$lib/components/ui/textarea/textarea.svelte';
 
 	let response = '';
 	let completedResponse = '';
-	let userMessage = '';
+	let systemMessage = 'You are a helpful assistant';
+	let userMessage = 'who are you?';
 	let stream: ReadableStream | null = null;
 
-	async function sendData() {
+	async function generateSyntheticData() {
 		response = '';
 
 		if (stream) {
@@ -18,7 +24,7 @@
 			headers: {
 				'Content-Type': 'application/json'
 			},
-			body: JSON.stringify({ userMessage })
+			body: JSON.stringify({ systemMessage, userMessage })
 		});
 
 		if (responseStream.body) {
@@ -49,4 +55,35 @@
 	}
 </script>
 
-<Badge class="bg-red-500">Red Badge</Badge>
+<div class="hidden h-full flex-col md:flex w-full">
+	<div
+		class="container flex flex-col items-start justify-between space-y-2 py-4 sm:flex-row sm:items-center sm:space-y-0 md:h-16"
+	>
+		<h2 class="text-lg font-semibold">FineTuner</h2>
+		<div class="flex flex-row gap-x-4">
+			<Button>Download File</Button>
+			<Avatar.Root>
+				<Avatar.Fallback>TT</Avatar.Fallback>
+			</Avatar.Root>
+		</div>
+	</div>
+	<Separator />
+	<div class="flex flex-row w-full">
+		<div class="bg-gray-50 w-full">
+			<div class="max-w-lg flex flex-col gap-y-4 p-8">
+				<Label>{completedResponse}</Label>
+				{#if completedResponse}
+					<Button on:click={() => (response = '')}>Clear</Button>
+				{/if}
+			</div>
+		</div>
+		<div>
+			<div class="max-w-lg flex flex-col gap-y-4 p-8 min-w-[480px]">
+				<Textarea bind:value={systemMessage} placeholder="System Message" />
+				<Textarea bind:value={userMessage} placeholder="User Message" />
+
+				<Button on:click={generateSyntheticData}>Generate Synthetic Data</Button>
+			</div>
+		</div>
+	</div>
+</div>
